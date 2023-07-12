0ba127b922943d292d44e99f4791715f5fb59b5f
彭亚伦 <a@iy.rs>
2023-07-12 18:14:25 +0800

    solve problem 392

--- src/double_pointers/_392_is_subsequence.rs
+++ src/double_pointers/_392_is_subsequence.rs
@@ -2,33 +2,35 @@ struct Solution;
 
 impl Solution {
     pub fn is_subsequence(s: String, t: String) -> bool {
-        let mut s_chars = s.chars();
-        let mut t_chars = t.chars();
-
-        loop {
-            let s_char = s_chars.next();
-            let t_char = t_chars.next();
-            if s_char.is_none() {
-                return true;
-            }
-            if t_char.is_none() {
+        let mut s_iter = s.chars();
+        let mut t_iter = t.chars();
+        while let Some(s_char) = s_iter.next() {
+            if let Some(t_char) = t_iter.find(|&x| x == s_char) {
+                if s_char != t_char {
+                    return false;
+                }
+            } else {
                 return false;
             }
-            if s_char == t_char {
-                continue;
-            } else {
-                t_chars = t.chars();
-            }
         }
+       return true; 
     }
 }
 
 /// Top Voted Solution
-// struct SolutionTop;
-
-// impl SolutionTop {
-
-// }
+struct SolutionTop;
+impl SolutionTop {
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        let mut iter = t.chars();
+        for c in s.chars() {
+          match iter.find(|&p| p == c) {
+            Some(_) => (),
+            None => return false
+          }
+        }
+        true
+    }
+}
 
 #[test]
 fn test_392() {
@@ -39,13 +41,25 @@ fn test_392() {
     let s = String::from("axc");
     let t = String::from("ahbgdc");
     assert_eq!(Solution::is_subsequence(s, t), false);
-
+    
     let s = String::from("acb");
     let t = String::from("ahbgdc");
     assert_eq!(Solution::is_subsequence(s, t), false);
+
+
 }
 
-// #[test]
-// fn test_X_top() {
+#[test]
+fn test_392_top() {
+    let s = String::from("abc");
+    let t = String::from("ahbgdc");
+    assert_eq!(SolutionTop::is_subsequence(s, t), true);
+    let s = String::from("axc");
+    let t = String::from("ahbgdc");
+    assert_eq!(SolutionTop::is_subsequence(s, t), false);
+    
+    let s = String::from("acb");
+    let t = String::from("ahbgdc");
+    assert_eq!(SolutionTop::is_subsequence(s, t), false);
+}
 
-// }
